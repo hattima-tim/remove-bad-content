6859a9de81750af79a338fded9eaaf01bf80533e
hattima-tim <irfanridwan20@gmail.com>
2024-06-30 11:36:02 +0600

    add empty state in history page

--- src/history.ts
+++ src/history.ts
@@ -18,40 +18,53 @@ async function createNewsHistoryTable() {
   };
 
   const container = document.getElementById("tableContainer") as HTMLDivElement;
-  const table = document.createElement("table");
 
-  const headerRow = document.createElement("tr");
-  headers.forEach((headerText) => {
-    const header = document.createElement("th");
-    header.textContent = headerText;
-    headerRow.appendChild(header);
-  });
-  table.appendChild(headerRow);
+  if (removedNews.length > 0) {
+    const table = document.createElement("table");
 
-  removedNews.reverse().forEach((newsData) => {
-    const row = document.createElement("tr");
-
-    Object.entries(newsData).forEach(([key, value]) => {
-      const cell = document.createElement("td");
-
-      if (key === "date" || key === "hostname") {
-        cell.id = key;
-        cell.textContent = value;
-      } else {
-        const link = document.createElement("a");
-        link.href = value.url;
-        link.textContent = value.text;
-
-        cell.id = key;
-        cell.appendChild(link);
-      }
-
-      row.appendChild(cell);
+    const headerRow = document.createElement("tr");
+    headers.forEach((headerText) => {
+      const header = document.createElement("th");
+      header.textContent = headerText;
+      headerRow.appendChild(header);
     });
-    table.appendChild(row);
-  });
+    table.appendChild(headerRow);
 
-  container.appendChild(table);
+    removedNews.reverse().forEach((newsData) => {
+      const row = document.createElement("tr");
+
+      Object.entries(newsData).forEach(([key, value]) => {
+        const cell = document.createElement("td");
+
+        if (key === "date" || key === "hostname") {
+          cell.id = key;
+          cell.textContent = value;
+        } else {
+          const link = document.createElement("a");
+          link.href = value.url;
+          link.textContent = value.text;
+
+          cell.id = key;
+          cell.appendChild(link);
+        }
+
+        row.appendChild(cell);
+      });
+      table.appendChild(row);
+    });
+
+    container.appendChild(table);
+  } else {
+    const emptyPageMainText = document.createElement("h2");
+    emptyPageMainText.textContent = "No news removed yet";
+
+    const emptyPageSubtext = document.createElement("p");
+    emptyPageSubtext.textContent =
+      "Keep using the extension to see removed news here";
+
+    container.classList.add("empty");
+    container.appendChild(emptyPageMainText);
+    container.appendChild(emptyPageSubtext);
+  }
 }
-
 createNewsHistoryTable();
